b6944ace68a59a201299aad8ef11f5c9919762d2
liliang <1220795758@qq.com>
2024-06-30 22:27:32 +0800

    fixed: 进入聊天室，离开聊天室处理

--- src/main.js
+++ src/main.js
@@ -1,22 +1,60 @@
 import WebSocket, { WebSocketServer } from "ws";
 import { PORT } from "./config.js";
+import { messageType } from "./messageType.js";
 
 const wss = new WebSocketServer({ port: PORT });
 
-wss.on("connection", function connection(ws) {
-  console.log("有人进入聊天室");
+const userList = [];
 
-  ws.on("error", console.error);
+// 聊天室广播
+const broadcast_each = (callback) => {
+  userList.forEach((user) => {
+    callback(user);
+  });
+};
 
-  ws.on("message", function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    const msg = JSON.parse(data.toString());
+
+    // 有人进入聊天室
+    if (msg.type == messageType.ENTER_ROOM_MSG) {
+      // 聊天室存在同名用户
+      if (userList.map((user) => user.username).includes(msg.sender)) {
+        ws.send(JSON.stringify({ type: messageType.ENTER_ROOM_MSG_FAILED }));
+        return;
       }
-    });
+
+      userList.push({ username: msg.sender, ws });
+      console.log(`${new Date().toLocaleString()}: ${msg.sender}进入聊天室`);
+
+      broadcast_each((user) => {
+        user.ws.send(
+          JSON.stringify({ type: messageType.ENTER_ROOM_MSG, username: msg.sender, userCount: userList.length })
+        );
+      });
+    }
+
+    // 有人离开聊天室
+    if (msg.type == messageType.LEAVE_ROOM_MSG) {
+      console.log(`${new Date().toLocaleString()}: ${msg.sender}离开聊天室`);
+      const index = userList.map((user) => user.username).indexOf(msg.sender);
+      if (index >= 0) {
+        userList.splice(index, 1);
+      }
+      broadcast_each((user) => {
+        user.ws.send(
+          JSON.stringify({ type: messageType.LEAVE_ROOM_MSG, username: msg.sender, userCount: userList.length })
+        );
+      });
+    }
+
+    // 有人发送消息
+    if (msg.type == messageType.PLAIN_TEXT_MSG || msg.type == messageType.RICH_TEXT_MSG) {
+    }
   });
 });
 
 wss.on("listening", () => {
-  console.log(`服务启动于 ws:127.0.0.1:${PORT}`);
+  console.log(`服务启动于 ws://127.0.0.1:${PORT}`);
 });
---
+++ src/messageType.js
@@ -0,0 +1,7 @@
+export const messageType = {
+  ENTER_ROOM_MSG: 0,
+  ENTER_ROOM_MSG_FAILED: 1,
+  LEAVE_ROOM_MSG: 2,
+  PLAIN_TEXT_MSG: 3,
+  RICH_TEXT_MSG: 4
+};
