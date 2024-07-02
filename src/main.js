8e9ca8a8cb985f69085cfda8fb5b2e56ded9929b
liliang <1220795758@qq.com>
2024-07-02 21:06:30 +0800

    fixed: 客户端关闭逻辑补充

--- src/main.js
+++ src/main.js
@@ -26,38 +26,64 @@ wss.on("connection", (ws) => {
       }
 
       // 进入成功
-      ws.send(
-        JSON.stringify({ type: messageType.ENTER_ROOM_SUCCESS, username: msg.sender, userCount: userList.length + 1 })
-      );
+      ws.send(JSON.stringify({ type: messageType.ENTER_ROOM_SUCCESS, username: msg.sender }));
 
-      // 广播给聊天室其他成员
+      userList.push({ username: msg.sender, ws });
+      // 聊天室广播(包括自己)
       broadcast_each((user) => {
         user.ws.send(
-          JSON.stringify({ type: messageType.ENTER_ROOM_MSG, username: msg.sender, userCount: userList.length + 1 })
+          JSON.stringify({ type: messageType.SOMEONE_ENTER_ROOM, username: msg.sender, userCount: userList.length })
         );
       });
-      userList.push({ username: msg.sender, ws });
 
       console.log(`${new Date().toLocaleString()}: ${msg.sender}进入聊天室`);
     }
 
     // 有人离开聊天室
     else if (msg.type == messageType.SOMEONE_LEAVE_ROOM) {
-      console.log(`${new Date().toLocaleString()}: ${msg.sender}离开聊天室`);
+      // 聊天室广播(包括自己)
+      broadcast_each((user) => {
+        user.ws.send(
+          JSON.stringify({ type: messageType.SOMEONE_LEAVE_ROOM, username: msg.sender, userCount: userList.length - 1 })
+        );
+      });
+
       const index = userList.map((user) => user.username).indexOf(msg.sender);
       if (index >= 0) {
         userList.splice(index, 1);
       }
-      broadcast_each((user) => {
-        user.ws.send(
-          JSON.stringify({ type: messageType.SOMEONE_LEAVE_ROOM, username: msg.sender, userCount: userList.length })
-        );
-      });
+
+      console.log(`${new Date().toLocaleString()}: ${msg.sender}离开聊天室`);
     }
 
     // 有人发送消息
     else if (msg.type == messageType.PLAIN_TEXT_MSG || msg.type == messageType.RICH_TEXT_MSG) {
     }
+  });
+
+  // 客户端关闭
+  ws.on("close", () => {
+    let closeUserIndex = -1;
+    let closeUsername = "未定义";
+    userList.forEach((user, index) => {
+      if (user.ws === ws) {
+        closeUserIndex = index;
+        closeUsername = user.username;
+      }
+    });
+
+    if (closeUserIndex >= 0) {
+      userList.splice(closeUserIndex, 1);
+    }
+
+    // 聊天室广播
+    broadcast_each((user) => {
+      user.ws.send(
+        JSON.stringify({ type: messageType.SOMEONE_LEAVE_ROOM, username: closeUsername, userCount: userList.length })
+      );
+    });
+
+    console.log(`${new Date().toLocaleString()}: ${closeUsername}离开聊天室`);
   });
 });
 
